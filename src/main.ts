50089addef8e00591620ab56e0fa58aeb28f3829
junkor-1011 <tomoyukizxy@gmail.com>
2024-10-17 00:06:20 +0900

    multi process

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,18 @@
-console.log("hello, world");
+import cluster from "node:cluster";
+import { availableParallelism } from "node:os";
+import process from "node:process";
+
+if (cluster.isPrimary) {
+  const numCPUs = availableParallelism();
+  console.debug(`Primary process, pid: ${process.pid}`);
+
+  for (let i = 0; i < numCPUs; i++) {
+    const _worker = cluster.fork();
+  }
+
+  console.log("hello, world");
+} else if (cluster.isWorker) {
+  console.debug(`sub process, pid: ${process.pid}`);
+
+  process.kill(process.pid);
+}
